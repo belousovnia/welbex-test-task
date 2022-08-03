8a830a9ac7b03de90a512d78f03eb46e0a785c51
belousovnia <belousovnia@gmail.com>
2022-08-03 20:52:08 +0400

    modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import MainPage from "./components/MainPage";
-import { Routes, Route, useLocation} from 'react-router-dom';
+import { Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
